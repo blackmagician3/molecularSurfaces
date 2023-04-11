b0acdda5fa2a7549c6651ca1ff4b5d6d6614e2ea
Luka Blümler <luka.bluemler@uni-jena.de>
2023-04-11 22:06:08 +0200

    Basic template for project structure and cmake

---
+++ app/main.cu
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main()
+{
+  std::cout << "is it working?" << std::endl;
+
+  // Bring in the dummy class from the example source,
+  // just to show that it is accessible from main.cpp.
+}
